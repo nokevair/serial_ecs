63bbd3572e085bd44ea556edae1475d55744372d
nokevair <64569057+nokevair@users.noreply.github.com>
2020-07-07 00:50:58 -0700

    add world state to lua registry on init
    - make world_data_ref Sendable by using Arc<RwLock> instead of Rc<RefCell>
    - track the registry key for the Lua world state

--- src/lua.rs
+++ src/lua.rs
@@ -1,31 +1,47 @@
-use rlua::Lua;
+use rlua::{Lua, RegistryKey};
 
-use std::cell::RefCell;
 use std::io;
-use std::rc::Rc;
+use std::sync::{Arc, RwLock, PoisonError};
 
 use crate::decode;
 use crate::encode;
 use crate::error;
 use crate::world::WorldData;
 
-#[derive(Default)]
-struct WorldDataRef(Rc<RefCell<WorldData>>);
+#[derive(Default, Clone)]
+struct WorldDataRef(Arc<RwLock<WorldData>>);
 
 pub struct World {
     lua: Lua,
+    data_ref_key: RegistryKey,
+
     data: WorldDataRef,
 }
 
+impl rlua::UserData for WorldDataRef {}
+
 impl World {
+    fn register_data_ref(
+        lua: &Lua,
+        data_ref: WorldDataRef
+    ) -> rlua::Result<RegistryKey> {
+        lua.context(|ctx| {
+            ctx.create_registry_value(data_ref)
+        })
+    }
+
     pub fn new() -> Self {
         Self::with_lua(Lua::new())
     }
 
     pub fn with_lua(lua: Lua) -> Self {
+        let data = WorldDataRef::default();
+        let data_ref_key = Self::register_data_ref(&lua, data.clone())
+            .expect("failed to add world data to Lua registry");
         Self {
             lua,
-            data: WorldDataRef::default(),
+            data_ref_key,
+            data,
         }
     }
 
@@ -37,15 +53,17 @@ impl World {
         reader: R,
         lua: Lua
     ) -> Result<Self, error::DecodeError> {
-        let world_data = decode::State::new(reader).decode_world()?;
-        Ok(Self {
-            lua,
-            data: WorldDataRef(Rc::new(RefCell::new(world_data))),
-        })
+        let data = decode::State::new(reader).decode_world()?;
+        let data = WorldDataRef(Arc::new(RwLock::new(data)));
+        let data_ref_key = Self::register_data_ref(&lua, data.clone())
+            .expect("failed to add world data to Lua registry");
+        
+        Ok(Self { lua, data_ref_key, data })
     }
 
     pub fn to_writer<W: io::Write>(&self, writer: W) -> io::Result<()> {
+        let world = self.data.0.read().unwrap_or_else(PoisonError::into_inner);
         encode::State::new(writer)
-            .encode_world(&self.data.0.borrow())
+            .encode_world(&world)
     }
 }
