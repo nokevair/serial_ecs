4dadcab04ef4577bec7d9a083da436349d1c6e5e
nokevair <64569057+nokevair@users.noreply.github.com>
2020-07-08 17:08:06 -0700

    add query manipulation methods

--- src/lua/script.rs
+++ src/lua/script.rs
@@ -16,7 +16,7 @@ pub enum System {
 }
 
 pub enum Query<Q> {
-    Lua(RegistryKey, Box<dyn FnMut(&rlua::Value) -> Q>),
+    Lua(RegistryKey, Box<dyn FnMut(rlua::Value) -> Q>),
     Native(Box<dyn FnMut(&mut WorldContext) -> Q>),
 }
 
@@ -87,6 +87,61 @@ impl<ID, Q> World<ID, Q> where ID: Hash + Eq {
     }
 
     pub fn clear_systems(&mut self) {
+        self.systems.clear();
+        self.lua.context(|ctx| ctx.expire_registry_values());
+    }
+
+    pub fn register_lua_query(
+        &mut self,
+        id: ID,
+        code: &[u8],
+        post_process: impl FnMut(rlua::Value) -> Q + 'static,
+    ) -> rlua::Result<ScriptType> {
+        let key = self.lua.context(|ctx| {
+            let query_fn: rlua::Function = ctx.load(code).eval()?;
+            ctx.create_registry_value(query_fn)
+        })?;
+        let old = self.queries.insert(id, Query::Lua(key, Box::new(post_process)));
+        Ok(ScriptType::from_opt_query(old.as_ref()))
+    }
+
+    pub fn register_native_query(
+        &mut self,
+        id: ID,
+        func: impl FnMut(&mut WorldContext) -> Q + 'static
+    ) -> ScriptType {
+        let old = self.queries.insert(id, Query::Native(Box::new(func)));
+        ScriptType::from_opt_query(old.as_ref())
+    }
+
+    pub fn run_query(&mut self, id: &ID) -> rlua::Result<Option<Q>> {
+        match self.queries.get_mut(id) {
+            None => Ok(None),
+            Some(Query::Lua(key, post_process)) => {
+                let ctx_ref_key = &self.ctx_ref_key;
+                self.lua.context(|ctx| {
+                    let system_fn: rlua::Function = ctx.registry_value(key)?;
+                    let ctx_ref: rlua::Value = ctx.registry_value(&ctx_ref_key)?;
+                    let result: rlua::Value = system_fn.call(ctx_ref)?;
+                    Ok(Some(post_process(result)))
+                })
+            }
+            Some(Query::Native(ref mut func)) => {
+                let mut world = self.ctx_ref.write();
+                Ok(Some(func(&mut *world)))
+            }
+        }
+    }
+
+    pub fn query_info(&self, id: &ID) -> ScriptType {
+        ScriptType::from_opt_query(self.queries.get(id))
+    }
+
+    pub fn remove_query(&mut self, id: &ID) -> ScriptType {
+        ScriptType::from_opt_query(self.queries.remove(id).as_ref())
+    }
+
+    pub fn clear_queries(&mut self) {
         self.systems.clear();
         self.lua.context(|ctx| ctx.expire_registry_values());
     }
